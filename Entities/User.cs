1fd993dab333d2b93d187f2fb08dd0b774e5be1a
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-03-10 13:13:05 +0100

    User entitás létrehozása

---
+++ Entities/User.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingGarageAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
