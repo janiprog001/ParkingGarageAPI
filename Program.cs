c797f1a0b34e05ed45adebf85b0195b66d178832
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-03-10 12:58:13 +0100

    4. lépés: Swagger API dokumentáció konfigurálása

--- Program.cs
+++ Program.cs
@@ -14,3 +14,31 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.LoginPath = "/api/users/login";
         options.LogoutPath = "/api/users/logout";
     });
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("cookieAuth", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        In = Microsoft.OpenApi.Models.ParameterLocation.Cookie,
+        Name = ".AspNetCore.Cookies", // A cookie neve az alapértelmezett .NET Authentication séma esetén
+        Description = "Paste your authentication cookie here"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "cookieAuth"
+                }
+            },
+            new List<string>()
+        }
+    });
+});
