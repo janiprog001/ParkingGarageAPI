f768132fbae957ff0029a4b656b85f21a27a440f
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-03-10 13:08:20 +0100

    Entitások közötti kapcsolatok konfigurálása az ApplicationDbContext-ben

--- Context/ApplicationDbContext.cs
+++ Context/ApplicationDbContext.cs
@@ -10,5 +10,16 @@ namespace ParkingGarageAPI.Context
 
         public DbSet<User> Users { get; set; }
         public DbSet<Car> Cars { get; set; }
+
+        // Az entitások közötti kapcsolat konfigurálása
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Car>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Cars)
+                .HasForeignKey(c => c.UserId);
+        }
     }
 }
