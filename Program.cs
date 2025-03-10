2db4d91dbbe0b6afd8dc06146b9dc2a77ac7fb24
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-03-10 13:50:12 +0100

    WeatherForecast rekord eltávolítása

--- Program.cs
+++ Program.cs
@@ -55,8 +55,3 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
