686f85b7774cb1328ec8726a87a990e87a26677f
janiprog001 <147839929+janiprog001@users.noreply.github.com>
2025-03-10 12:54:40 +0100

    2. lépés: Adatbázis-kontextus konfigurálása

--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using ParkingGarageAPI.Context;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("ParkingGarageDB"));
